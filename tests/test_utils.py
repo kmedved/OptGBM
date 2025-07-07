05598e7c4b46130f380b715413b59cc2b8871053
kmedved <bibigon@gmail.com>
2025-07-07 18:51:01 -0400

    Replace optuna internal alias imports

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,20 @@
 import numpy as np
 
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_diabetes
 
 from optgbm.utils import check_fit_params
 from optgbm.utils import check_X
 
 
 def test_check_X() -> None:
-    X, _ = load_boston(return_X_y=True)
+    X, _ = load_diabetes(return_X_y=True)
     X = check_X(X)
 
     assert isinstance(X, np.ndarray)
 
 
 def test_check_fit_params() -> None:
-    X, y = load_boston(return_X_y=True)
+    X, y = load_diabetes(return_X_y=True)
     X, y, sample_weight = check_fit_params(X, y)
 
     assert isinstance(X, np.ndarray)
